6bd0e411fc03eb9f71997f699ffe45d742edf63e
annazuzu <maxann89@gmail.com>
2021-08-29 08:12:16 +0300

    Ex13: User Agent

--- preps_and_tests/some_headers.py
+++ preps_and_tests/some_headers.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 responce = requests.get("https://playground.learnqa.ru/api/homework_header")
---
+++ tests/test_ex13_user_agent.py
@@ -0,0 +1,64 @@
+import pytest
+import requests
+
+
+class TestEx13UserAgent:
+    user_agents = [
+        "Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+        "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 "
+        "Mobile/15E148 Safari/604.1",
+        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+        "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"
+    ]
+
+    expected_values = [
+        {'platform': 'Mobile', 'browser': 'No', 'device': 'Android'},
+        {'platform': 'Mobile', 'browser': 'Chrome', 'device': 'iOS'},
+        {'platform': 'Googlebot', 'browser': 'Unknown', 'device': 'Unknown'},
+        {'platform': 'Web', 'browser': 'Chrome', 'device': 'No'},
+        {'platform': 'Mobile', 'browser': 'No', 'device': 'iPhone'}
+    ]
+
+    testdata = [
+        (user_agents[0], expected_values[0]['platform'], expected_values[0]['browser'], expected_values[0]['device']),
+        (user_agents[1], expected_values[1]['platform'], expected_values[1]['browser'], expected_values[1]['device']),
+        (user_agents[2], expected_values[2]['platform'], expected_values[2]['browser'], expected_values[2]['device']),
+        (user_agents[3], expected_values[3]['platform'], expected_values[3]['browser'], expected_values[3]['device']),
+        (user_agents[4], expected_values[4]['platform'], expected_values[4]['browser'], expected_values[4]['device'])
+    ]
+
+    def assert_this(self, actual_value, expected_value, value):
+        assert actual_value == expected_value, f"Actual {value} {actual_value} is not equal to expected {value} {expected_value}"
+
+    @pytest.mark.parametrize('user_agent, expected_platform, expected_browser, expected_device', testdata,
+                             ids=[1, 2, 3, 4, 5])
+    def test_responce_user_agent(self, user_agent, expected_platform, expected_browser, expected_device):
+        headers = {"User-Agent": user_agent}
+
+        responce = requests.get("https://playground.learnqa.ru/ajax/api/user_agent_check", headers=headers)
+
+        code = responce.status_code
+        assert code == 200, f"Wrong responce code - {code}"
+
+        responce_dict = responce.json()
+
+        assert "platform" in responce_dict, "There is no field 'platform' in the responce"
+        assert "browser" in responce_dict, "There is no field 'browser' in the responce"
+        assert "device" in responce_dict, "There is no field 'device' in the responce"
+
+        actual_platform = responce_dict["platform"]
+        actual_browser = responce_dict["browser"]
+        actual_device = responce_dict["device"]
+
+        print("actual_platform: " + actual_platform)
+        print("actual_browser: " + actual_browser)
+        print("actual_device: " + actual_device)
+
+        self.assert_this(actual_platform, expected_platform, "platform")
+        self.assert_this(actual_browser, expected_browser, "browser")
+        self.assert_this(actual_device, expected_device, "device")
+
+
+
+
